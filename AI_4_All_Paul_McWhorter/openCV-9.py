5d9c2e45ac7f0c6022d3faaa29a2ad2fafe7cf3e
realdump <a.majeed17@outlook.com>
2023-11-29 14:35:27 +0530

    update

--- AI_4_All_Paul_McWhorter/openCV-9.py
+++ AI_4_All_Paul_McWhorter/openCV-9.py
@@ -1,14 +1,20 @@
 import cv2
 print(cv2.__version__)
-width=1280
-height=720
-cam=cv2.VideoCapture(0,cv2.CAP_DSHOW)
+width=640
+height=360
+cam=cv2.VideoCapture(0)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
 cam.set(cv2.CAP_PROP_FPS, 30)
 cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
 while True:
     ignore,  frame = cam.read()
+    frameROI=frame[150:210,250:390]
+    frameROIGray=cv2.cvtColor(frameROI,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('my ROIGRAY',frameROIGray)
+    cv2.moveWindow('my ROIGRAY', 650,100)
+    cv2.imshow('my ROI',frameROI)
+    cv2.moveWindow('my ROI', 650,0)
     cv2.imshow('my WEBcam', frame)
     cv2.moveWindow('my WEBcam',0,0)
     if cv2.waitKey(1) & 0xff ==ord('q'):
