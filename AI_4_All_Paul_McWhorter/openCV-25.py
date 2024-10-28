c666d82fd4a2883af6750125bcb7f985b3aba539
realdump <a.majeed17@outlook.com>
2024-10-28 19:27:28 +0530

    update

---
+++ AI_4_All_Paul_McWhorter/openCV-25.py
@@ -0,0 +1,11 @@
+import os
+import cv2
+import face_recognition as FR
+print(cv2.__version__)
+imageDir='/home/abdul/Documents/GitHub/AI_Examples/AI_Examples/AI_4_All_Paul_McWhorter/demoImages/known'
+for root,dirs,files in os.walk(imageDir):
+    print('my present directory(root):', root)
+    print('Dirs in root', dirs)
+    print('My Files in root', files)
+    for file in files:
+        print('Your Guy Is:', file)
